3f2381d35019f3a582cf9e51cdaff0eb7597d294
shreypilot <shreypilot28@gmail.com>
2024-02-05 01:34:55 +0530

    Reinitialization

---
+++ Re-initialization.js
@@ -0,0 +1,10 @@
+//Reinitialization
+//okay with var 
+var a = 5 ;
+a = 6;
+//okay with let 
+let b = 5;
+b=10;
+//but not okay with const ->it throw typeError:- Assignment to constant variable 
+const c = 2;
+c = 10 ;
