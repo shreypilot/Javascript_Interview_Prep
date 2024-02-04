7e785daedf2c354447c26bb07ca0a9db7be071cc
shreypilot <shreypilot28@gmail.com>
2024-02-05 02:18:31 +0530

    map

---
+++ Map.js
@@ -0,0 +1,7 @@
+//used for creating a new array from existing array by applying function to each of the element of first arrays
+
+const nums = [1,2,3,4];
+const multiplyThree = nums.map((nums,i)=>{
+    return nums*3+i;
+})
+console.log(multiplyThree);
