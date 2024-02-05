5fd8914c4e611884d856b93aec745376c81c583c
shreypilot <shreypilot28@gmail.com>
2024-02-05 19:34:14 +0530

    params Vs Arguments

---
+++ ParamsVsArguments.js
@@ -0,0 +1,5 @@
+function square(num){//params -> the variables that are defined when the function is declared 
+    console.log(num *num);
+}
+
+square(5);//Arguments -> The values that are declared within a function when the function is called
