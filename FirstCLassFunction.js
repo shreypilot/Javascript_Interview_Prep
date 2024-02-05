efe16b73de88b0e5d5e0aeaf17cdb54f1b3588fd
shreypilot <shreypilot28@gmail.com>
2024-02-05 13:59:07 +0530

    FirstClassFunction

---
+++ FirstCLassFunction.js
@@ -0,0 +1,14 @@
+//FirstClassFunction -> Function can be treated like variable their function are called first class function
+
+//Inthis case function can be pass into another function ,can be used manipulated and return from those function
+//Basically-> do everything which is variable can do 
+
+function square (num){
+    return num*num;
+}
+
+function displaySquare (fn){
+    console.log("Square is "+fn(5));
+}
+
+displaySquare(square);
