80895d111ce891da146a528807d5162b7a3efde5
shreypilot <shreypilot28@gmail.com>
2024-02-05 19:31:54 +0530

    Function EX and dec

--- Function.js
+++ Function.js
@@ -1,3 +1,6 @@
+//Function -> A JavaScript function is a block of code designed 
+//to perform a particular task. A JavaScript function is executed when "something" invokes it (calls it).
+
 //Function declaration is function definition or statement 
 
 function square1 (nums){
