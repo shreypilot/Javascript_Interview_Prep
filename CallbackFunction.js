5b68db2d56f781cfdd58ed2dc0ad6b5c841b94bc
shreypilot <shreypilot28@gmail.com>
2024-02-06 16:39:17 +0530

    callback

---
+++ CallbackFunction.js
@@ -0,0 +1,7 @@
+//Callback function is a function which passed into another function as an arguments ,
+// which  is then invoked inside the outer function to complete action
+
+//Ex.:- document.addEventListener("click",function(params)){
+
+//});
+
